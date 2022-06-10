6ebc558a213d8179630de10ca549f8cd29c3eb6b
paxasaval <paxasaval1003@gmail.com>
2022-06-10 03:01:40 -0500

    museos main view

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,7 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button'
-
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
@@ -16,7 +16,8 @@ import {MatButtonModule} from '@angular/material/button'
     MatFormFieldModule,
     HttpClientModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   exports:[
     FormsModule,
@@ -24,7 +25,8 @@ import {MatButtonModule} from '@angular/material/button'
     MatFormFieldModule,
     HttpClientModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
 
   ]
 })
