8aaa512656f5291a66369f6e14b3099fb4a22167
dpmerino <diegopaul7@outlook.com>
2022-06-17 09:50:48 -0500

    update and delete function created for museums staff and rol

---
+++ src/app/services/roles.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
+import { Rol, RolId } from '../models/rol';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RolesService {
+
+  private rolCollection: AngularFirestoreCollection<Rol>;
+  roles: Observable<Rol[]>;
+
+  constructor(
+    private afs: AngularFirestore
+  ) {
+    this.rolCollection = afs.collection<Rol>('roles');
+    this.roles = this.rolCollection.valueChanges();
+  }
+
+  getAllRoles() {
+    return this.afs.collection<Rol>('roles').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as RolId
+        data.id = a.payload.doc.id
+        return data
+      }))
+    )
+  }
+
+  getRolByName(name: string) {
+    return this.afs.collection<Rol>('roles', ref => ref.where('name', '==', name)).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as RolId
+        data.id = a.payload.doc.id
+        return data
+      }))
+    )
+  }
+
+  updateRol(rol: RolId) {
+    const rolReference = this.afs.doc<Rol>(`roles/${rol.id}`)
+    rolReference.set(rol)
+  }
+
+  deleteRol(rol: RolId) {
+    this.afs.doc<Rol>('roles/' + rol.id).delete();
+  }
+}
