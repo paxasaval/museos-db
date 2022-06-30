8130ace38a7cd35fd50b2a099f51e51b9880c294
dpmerino <diegopaul7@outlook.com>
2022-06-30 10:36:38 -0500

    summary model/interface

---
+++ src/app/models/summary.ts
@@ -0,0 +1,56 @@
+import { Timestamp } from "firebase/firestore"
+
+export interface Region_visit {
+    region?: string,
+    visit?: number
+}
+
+export interface Country_visit {
+    country?: string,
+    total_visits?: number
+}
+
+export interface dataRegionsVisit {
+    region?: string,
+    countries?: Region_visit[],
+    visit?: number
+}
+
+export interface Place_record {
+    item_id?: string,
+    total_visits?: number
+}
+
+export interface Transport_visit {
+    item_id?: string,
+    total_visits?: number    
+}
+
+export interface LastMonth_visit {
+    month?: number,
+    total_visits?: number
+} 
+
+export interface LastYear_visit {
+    year?: string,
+    total_visits?: number
+}
+
+export interface Reason_visit {
+    item_id?: string,
+    total_visits?: number
+}
+
+export interface Summary {
+    cutoff_date?: Timestamp,
+    avg_tourist_visit?: number,
+    avg_visits_days?: number,
+    total_record?: number,
+    country_visit?: Country_visit[],
+    place_record?: Place_record[],
+    lastMonth_visit?: LastMonth_visit[],
+    lastYear_visit?: LastYear_visit[],
+    region_visit?: Region_visit[],
+    transport_visit?: Transport_visit[],
+}
+export interface SummaryId extends Summary { id: string }
