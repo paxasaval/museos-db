139119624b1661204b74bd496ec096780a6c5815
davidLM20 <jhandry13@hotmail.com>
2022-07-07 00:47:45 -0500

    Estilos registro

--- src/app/pages/musem-detail/dialog-r-visit/dialog-r-visit.component.ts
+++ src/app/pages/musem-detail/dialog-r-visit/dialog-r-visit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-r-visit',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogRVisitComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private dialogRef: MatDialogRef<DialogRVisitComponent >,
+  ) { }
 
   ngOnInit(): void {
   }
-
+  close(){
+    this.dialogRef.close()
+  }
 }
